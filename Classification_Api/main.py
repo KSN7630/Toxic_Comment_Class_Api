f5a6b603261f2c3db6605e9385dccebcb9a06556
Kartik <narkhede.1@iitj.ac.in>
2024-09-27 01:18:55 +0530

    Changing 1

--- Classification_Api/main.py
+++ Classification_Api/main.py
@@ -1,41 +1,32 @@
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# from pred import output_report
-# from trainedModels import load_models
-
-# app = FastAPI()
-# models=load_models()
-
-# class CommentRequest(BaseModel):
-#     input_string: str
-
-# @app.post("/predict-comment")
-# def comment_prediction(request: CommentRequest):
-#     input_string = request.input_string
-
-#     prediction_report = output_report(input_string,models)
-#     # Return the prediction result
-#     print(prediction_report)
-#     return prediction_report
-
-
-
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-# # if __name__ == "__main__":
-# #     import uvicorn
-# #     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
-
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from pred import output_report
+from trainedModels import load_models
 
 app = FastAPI()
+models=load_models()
+
+class CommentRequest(BaseModel):
+    input_string: str
+
+@app.post("/predict-comment")
+def comment_prediction(request: CommentRequest):
+    input_string = request.input_string
+
+    prediction_report = output_report(input_string,models)
+    # Return the prediction result
+    print(prediction_report)
+    return prediction_report
+
+
+
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI on Vercel"}
+    return {"Hello": "World"}
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
